876e2c59a9e797fa2595b83e72df0a26b0e57a51
Prashant Jondhale <jondhaleprashant@gmail.com>
2020-03-09 19:37:40 +0530

    code with simple listview

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'model/Note.dart';
 void main() => runApp(MyApp());
 DatabaseHelper databaseHelper;
 
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
@@ -23,12 +22,9 @@ class MyApp extends StatelessWidget {
       ),
       // home: MyHomePage(title: 'Notes App'),
       home: MyHomePage(title: 'notes App'),
-
     );
   }
 }
-
-
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -44,13 +40,14 @@ class MyHomePage extends StatefulWidget {
 class ShowDialogState extends State {
   TextEditingController noteController = TextEditingController();
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Notes App"),
-
       ),
+
       body: notesListView(context),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -67,6 +64,20 @@ class ShowDialogState extends State {
   }
 
   Widget notesListView(BuildContext context) {
+   /* if (_noteList != null) {
+      print("note list not null");
+      return ListView.builder(
+        itemCount: _noteList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_noteList.elementAt(index).note),
+          );
+        },
+      );
+    }
+    else{
+      print("note list null");
+    }*/
     return FutureBuilder<List<Note>>(
     future: getAllNotes(),
       builder: (BuildContext context, AsyncSnapshot<List<Note>> snapshot) {
@@ -88,10 +99,10 @@ class ShowDialogState extends State {
          }
       }
   );
+
   }
 
   showAddNoteDialog() {
-
     AlertDialog dialog = AlertDialog(
       title: Text("Add Note"),
       content: TextField(
@@ -135,8 +146,8 @@ class ShowDialogState extends State {
       Note note = new Note(note: noteController.text);
       databaseHelper.createNote(note);
       setState(() {
-
         notesListView(context);
+
       });
     }
     cancelPop();
@@ -149,29 +160,25 @@ class ShowDialogState extends State {
     Navigator.of(context).pop();
   }
 
- Future<List<Note>> getAllNotes() async {
+  Future<List<Note>> getAllNotes() async {
     print("in getAllNotes ");
-    List<Note> noteList=List();
-  if(databaseHelper!=null) {
-    print("database helper is not  null");
-     noteList = await databaseHelper.getNotes();
+    List<Note> noteList = List();
+    if (databaseHelper != null) {
+      print("database helper is not  null");
+      noteList = await databaseHelper.getNotes();
 
-    if (noteList != null) {
-      print("note list is not  null");
-      for (var value in noteList) {
-        print('note id is ${value.id} and note is ${value.note}');
+      if (noteList != null) {
+        print("note list is not  null");
+        for (var value in noteList) {
+          print('note id is ${value.id} and note is ${value.note}');
+        }
+      } else {
+        print("note list is null");
       }
-
-
+    } else {
+      print("database helper is null");
     }
-    else{
-      print("note list is null");
-    }
-  }
-  else{
-    print("database helper is null");
-  }
 
-  return noteList;
+    return noteList;
   }
 }
